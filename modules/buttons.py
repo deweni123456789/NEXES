2fa5f76e206694defa14095b1084608c1c9dd034
deweni123456789 <schoford91@gmail.com>
2025-09-14 09:58:02 +0530

    Create buttons.py

---
+++ modules/buttons.py
@@ -0,0 +1,10 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def make_uploaded_keyboard(bot_username: str):
+    developer = InlineKeyboardButton(text="👨‍💻 Developer", url="https://t.me/deweni2")
+    support = InlineKeyboardButton(text="💬 Support Group", url="https://t.me/slmusicmania")
+    contact = InlineKeyboardButton(text="📩 Contact Bot", url=f"https://t.me/{bot_username}")
+    return InlineKeyboardMarkup([[developer, support], [contact]])
+
+def make_start_keyboard(bot_username: str):
+    return make_uploaded_keyboard(bot_username)
