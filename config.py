9cc653cd2290ae7fc6aecbf015db85dd0725254c
deweni123456789 <schoford91@gmail.com>
2025-09-14 10:01:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Example config (use env vars in Railway)
-API_ID = 123456
-API_HASH = "your_api_hash_here"
-BOT_TOKEN = "123456:ABCDEF"
+API_ID = 5047271
+API_HASH = " 047d9ed308172e637d4265e1d9ef0c27"
+BOT_TOKEN = "8464050626:AAFjoldNU_A5jHEzSspCDDNUy5__WyEFfms"
 BOT_USERNAME = "YourBotUsername"  # without @
